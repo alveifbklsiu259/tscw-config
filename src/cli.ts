6527d62042018031e46e30fd7d600cc60971b68e
alveifbklsiu259 <alveifbklsiu259@gmail.com>
2024-08-12 18:11:11 +0800

    feat: add main script, cli script

---
+++ src/cli.ts
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+import fs from "node:fs"
+import { exec } from "child_process"
+import path from "node:path"
+
+const toPosixPath = (p: string) => p.split(path.win32.sep).join(path.posix.sep);
+const args = process.argv
+    .slice(2)
+    .map((arg) => toPosixPath(arg))
+    .join(" ");
+
+const script = path.join(__dirname, "../src/scripts/tsc_with_config.sh");
+const relativePathToScript = toPosixPath(path.relative(process.cwd(), script));
+
+const child = exec(
+    `bash ${relativePathToScript} ${args}`,
+    (error, stdout, stderr) => {
+        if (stderr) {
+            console.error(`Stderr - ${stderr}`);
+            return;
+        }
+        if (error) {
+            console.error(`Error - ${error.message}`);
+            return;
+        }
+        console.log(stdout);
+    }
+);
+
+/**
+ * On Windows, where POSIX signals do not exist, the signal argument will be ignored, and the process will be killed forcefully and abruptly (similar to 'SIGKILL').
+ * @see {@link https://nodejs.org/api/child_process.html#subprocesskillsignal}
+ *
+ * Because of this, the `trap 'rm -f -- "$tmp_file"' EXIT SIGINT SIGHUP SIGTERM` in bash script will not function well when any of the above signals is sent.
+ */
+
+const tempFileName = path.join(process.cwd(), "/tmp_ts_config_name.txt");
+
+const signals = ["exit", "SIGINT", "SIGHUP", "SIGTERM"] as const;
+
+for (const signal of signals ) {
+    process.on(signal, (exitCode) => {
+        if (fs.existsSync(tempFileName)) {
+            const tmpFile = fs.readFileSync(tempFileName, "utf8").trim();
+            if (fs.existsSync(tmpFile)) {
+                fs.unlinkSync(tmpFile);
+            }
+            fs.unlinkSync(tempFileName);
+        }
+        if (signal !== "exit") {
+            child.kill(signal);
+        }
+        // eslint-disable-next-line
+        process.exit(exitCode);
+    });
+}
---
+++ src/scripts/tsc_with_config.sh
@@ -0,0 +1,151 @@
+#!/usr/bin/env bash
+
+# The purpose of this script to solve the problem: "When input files are specified on the command line with "tsc", tsconfig.json files are ignored."
+find_nearest_ts_config() {
+	local dir
+	dir=$(pwd)
+	while [[ "$dir" != "/" ]]; do
+		if [[ -f "$dir/tsconfig.json" ]]; then
+			# Output the absolute path to tsconfig.json
+			printf "%s" "$(realpath "$dir/tsconfig.json")"
+			return 0
+		fi
+		# Update the value of the dir variable to its parent directory
+		dir=$(dirname "$dir")
+	done
+	return 1
+}
+
+ts_config=""
+declare -a flags=()
+declare -a files=()
+
+while [[ "$#" -gt 0 ]]; do
+	case $1 in 
+		-p|--project)
+			if [[ -z "$2" ]]; then
+				echo "Missing argument for $1" >&2
+				exit 1
+			else
+				if [[ -f $(realpath "$2") ]]; then
+					ts_config="$(realpath "$2")"
+					shift 2
+				else
+					echo "Can't find $(realpath "$2")" >&2
+					exit 1
+				fi
+			fi ;;
+		--excludeFiles)
+			if [[ -z "$2" ]]; then
+				echo "Missing argument for $1" >&2
+				exit 1
+			else
+				IFS=',' read -r -a excludeFiles_paths <<< "$2"
+
+				for path in "${excludeFiles_paths[@]}"; do
+					if [[ ! -f $(realpath "$path") ]]; then
+						echo "Can't find $(realpath "$path")" >&2
+						exit 1
+					fi
+				done
+				
+				flags+=("$1" "$2")
+				shift 2
+			fi ;;
+		# Other flags
+		-*) 
+			flags+=("$1")
+			shift ;;
+		*)
+			if [[ "$1" =~ \.(ts|cts|mts|tsx|js|cjs|mjs|jsx)$ ]]; then
+				# files
+				if [[ -f $(realpath "$1") ]]; then
+					files+=("$1")
+					shift
+				else
+					echo "Can't find $(realpath "$1")" >&2
+					exit 1
+				fi
+			else
+				# arg for the previous flag
+				flags+=("$1")
+				shift
+			fi ;;
+	esac
+done
+
+if [ -z "$ts_config" ]; then
+	ts_config=$(find_nearest_ts_config)
+fi
+
+if [ -z "$ts_config" ]; then
+	echo "tsconfig.json not found" >&2
+	exit 1
+fi
+
+ts_config_dir="$(realpath -s --relative-to="$(pwd)" "$(dirname "$ts_config")")"
+
+# Create a temporary file
+tmp_file=$(mktemp "$(dirname "$ts_config")"/tmp_tsconfig_XXXXXX.json)
+echo "$(realpath -s --relative-to="$(pwd)" $tmp_file)" > ./tmp_ts_config_name.txt
+trap 'rm -f -- "$tmp_file" ./tmp_ts_config_name.txt' EXIT SIGINT SIGHUP SIGTERM
+
+# Copy the contents of the tsconfig.json to the temporary file
+cp "$ts_config" "$tmp_file"
+
+rawData=$(<"$tmp_file")
+
+# Remove single-line comments (// comments)
+rawData=$(echo "$rawData" | sed 's|//.*$||g')
+
+# Remove multi-line comments (/* comments */)
+rawData=$(echo "$rawData" | sed -E ':a;N;$!ba;s|/\*[^*]*\*+([^/*][^*]*\*+)*/||g')
+
+echo "$rawData" > "$tmp_file"
+
+if [ "${#files[@]}" -lt 1 ]; then 
+	if ! grep --perl-regexp --null-data --quiet '"pretty"\s*:\s*(true|false)' "$tmp_file"; then
+		flags=("--pretty" "${flags[@]}")
+	fi
+	npx tsc --project "$ts_config_dir/$(basename "$tmp_file")" "${flags[@]}" 2>&1 &
+
+	wait
+	exit 0
+fi
+
+relative_paths_to_ts_config=""
+
+for file in "${files[@]}"; do
+	relative_paths_to_ts_config+="\"$(realpath -s --relative-to="$(dirname "$tmp_file")" "$file")\", "
+done
+
+# Remove the trailing comma and space
+relative_paths_to_ts_config=${relative_paths_to_ts_config%, }
+
+# Check if the "files" field exists (Match any number of spaces/tabs/newlines between the key and square brackets)
+if grep --perl-regexp --null-data --quiet '"files"\s*:\s*\[\s*[^]]*\s*\]' "$tmp_file"; then
+	# Replace the content inside the square brackets of the existing "files" field
+	# Instead of "/", use "|" as delimiter, because path may contain "/"
+	sed --null-data --in-place 's|"files"\s*:\s*\[[^]]*\]|"files": ['"$relative_paths_to_ts_config"']|' "$tmp_file"
+else
+	# Add the "files" field as the first field of the JSON object
+	sed -i '1s|{|{\n  "files": ['"$relative_paths_to_ts_config"'],|' "$tmp_file"
+fi
+
+# Remove include field (Match any number of spaces/tabs/newlines between the key and square brackets)
+if grep --perl-regexp --null-data --quiet '"include"\s*:\s*\[\s*[^]]*\s*\]' "$tmp_file"; then
+	sed --null-data --in-place 's|"include"\s*:\s*\[[^]]*\]\(,\)\?||g' "$tmp_file"
+fi
+
+# Pretty format
+if ! grep --perl-regexp --null-data --quiet '"pretty"\s*:\s*(true|false)' "$tmp_file"; then
+	sed -i 's|"compilerOptions"\s*:\s*{|"compilerOptions": {\n  "pretty": true,|' "$tmp_file"
+fi
+
+# No need to handle exclude field. see https://www.typescriptlang.org/tsconfig/#exclude
+
+# Forward CLI options
+npx tsc --project "$ts_config_dir/$(basename "$tmp_file")" "${flags[@]}" 2>&1 &
+
+wait 
+exit 0
