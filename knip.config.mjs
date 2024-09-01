b80df986cbeb2074d9a76d670090b7bd31df4445
alveifbklsiu259 <alveifbklsiu259@gmail.com>
2024-09-01 16:36:07 +0800

    chore: add cspell, knip, markdownlint

---
+++ knip.config.mjs
@@ -0,0 +1,11 @@
+//@ts-check
+
+/**
+ * @type {import("knip").KnipConfig}
+ */
+export default {
+	ignoreExportsUsedInFile: true,
+	includeEntryExports: true,
+	ignore: ["./test/fixtures/*.*", "./src/cleanupMonitor.ts", "./src/intermediate.ts"],
+	ignoreDependencies: ["@types/jest"],
+};
